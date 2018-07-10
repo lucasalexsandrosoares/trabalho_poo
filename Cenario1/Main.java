1051c1700163cf368430eb6de98ad44e5194e047
Lucas Alexsandro Soares <40281699+lucasalexsandrosoares@users.noreply.github.com>
2018-07-10 09:39:22 -0300

    Add files via upload

---
+++ Cenario1/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+	public static void main(String[] args) {
+		Cachorro c = new Cachorro();
+		Gato g = new Gato();
+		Aguia a = new Aguia();
+		c.latir();
+		g.dormir();
+		a.voar();
+		
+	}
+
+}
